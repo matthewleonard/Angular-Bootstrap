f2bf3d438a4ac4016bf6bc22ac3e9129190e486e
Matthew Leonard <matthew.leonard89@gmail.com>
2018-10-15 02:04:57 -0500

    first attempt at bootstrap. The page is still not rendering and without error messages. Going to try an updated version of bootstrap next time

--- blog/src/app/app.module.ts
+++ blog/src/app/app.module.ts
@@ -1,6 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { TooltipModule } from 'ngx-bootstrap/tooltip';
+import { ModalModule } from 'ngx-bootstrap/modal';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -8,7 +11,11 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    NgbModule,
+    BrowserModule,
+    BsDropdownModule,
+    TooltipModule,
+    ModalModule
   ],
   providers: [],
   bootstrap: [AppComponent]
